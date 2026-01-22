5f1565603eb90a0d9a42157310068849278ed881
Andre Jarboe <andrejarboe@gmail.com>
2026-01-21 18:32:20 -0600

    Fix typo in Game initialization method in Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,12 +6,13 @@
 #include <glm/glm.hpp>
 #include <imgui/imgui.h>
 #include <sol/sol.hpp>
+#include "Game.h"
 
 int main(int argc, char *argv[])
 {
     Game game;
 
-    game.intialize();
+    game.Initialize();
     game.Run();
     game.Destroy();
 
